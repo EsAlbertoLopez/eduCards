798a931b1aa2bf986e2190770795657bb078706e
Oswaldo Morales Rodriguez <A01378566@itesm.mx>
2020-09-19 11:13:46 -0500

    Se agrego la lista al fragmento
    Se despliega la lista de alumnos

--- app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentGeneral.kt
+++ app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentGeneral.kt
@@ -1,14 +1,33 @@
 package mx.itesm.rano.eduCards.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import mx.itesm.rano.eduCards.Interfaces.ListListener
 import mx.itesm.rano.eduCards.R
+import mx.itesm.rano.eduCards.models.Student
 
-class FragmentGeneral : Fragment() {
+class FragmentGeneral : ListFragment() {
 
+    var listener: ListListener? = null
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        listener?.itemClicked(position)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ListListener){
+            listener = context
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,6 +37,14 @@ class FragmentGeneral : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_general, container, false)
+        val vista = super.onCreateView(inflater, container, savedInstanceState)
+        val students = Array<String>(Student.students.size){
+            Student.students[it].name
+        }
+        val adaptador = ArrayAdapter<String>(inflater.context,
+            android.R.layout.simple_list_item_1 , students)
+
+        listAdapter = adaptador
+        return vista
     }
 }
