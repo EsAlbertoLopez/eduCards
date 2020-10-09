637cb7771ccb37eeafb73191d9797fcc6349b3e8
nplop <nplopez_s@hotmail.com>
2020-10-08 23:11:34 -0500

    adios

--- app/src/main/java/mx/itesm/rano/eduCards/adapters/AdapterHome.kt
+++ app/src/main/java/mx/itesm/rano/eduCards/adapters/AdapterHome.kt
@@ -38,7 +38,6 @@ class AdapterHome(var cardStatistics: Array<CardStatistics>) :
             rowView.tvCardStatistic.text = cardStatistic.cardStatistic
             rowView.tvCardAmount.text = cardStatistic.cardAmount
             //set color
-            println("Hola amigos jeje")
         }
     }
 }
