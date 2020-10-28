4d802aac00700f145d4af4f6aa805b33f3d5c1e3
Oswaldo Morales Rodriguez <A01378566@itesm.mx>
2020-10-28 16:15:21 -0600

    Update FragmentCardDetail.kt
    Ya quedo todo junto

--- app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentCardDetail.kt
+++ app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentCardDetail.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_card_detail.*
 import mx.itesm.rano.eduCards.R
+import mx.itesm.rano.eduCards.models.Card
 import mx.itesm.rano.eduCards.models.Student
 
 class FragmentCardDetail(selectedCourse: String, selectedGroup: String, selectedStudent: String, keyEvent: String) : Fragment() {
@@ -16,6 +23,8 @@ class FragmentCardDetail(selectedCourse: String, selectedGroup: String, selected
     var group = selectedGroup
     var student = selectedStudent
     var key = keyEvent
+    var cause = ""
+    var description = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,23 +32,51 @@ class FragmentCardDetail(selectedCourse: String, selectedGroup: String, selected
     }
 
     override fun onStart() {
-        
+        readFromcloud()
 
         super.onStart()
-        if(view != null) {
-            tvTitle.text = "Work"
-            tvDetail.text = "Work"
-            when("Violencia Física"){
-                "Violencia Física" ->  view!!.setBackgroundColor(Color.RED)
-                "Violencia Verbal" -> view!!.setBackgroundColor(Color.rgb(255, 69, 0))
-                "Comportamiento Inadecuado" -> view!!.setBackgroundColor(Color.rgb(255, 20, 147))
-                "Logro" -> view!!.setBackgroundColor(Color.YELLOW)
-                "Empatía" -> view!!.setBackgroundColor(Color.GREEN)
-                else-> view!!.setBackgroundColor(Color.CYAN)
 
-            }
-        }
     }
+
+    private fun readFromcloud() {
+        val courseId = course.split("[", "]")[1]
+        val groupId = group.split("[", "]")[1]
+        val studentId = student.split("[", "]")[1]
+        val database = FirebaseDatabase.getInstance()
+
+        val reference = database.getReference("/Courses/$courseId/Groups/$groupId/Alumnos/$studentId/Events/$key")
+        reference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val card = snapshot.getValue(Card::class.java)
+                if (card != null){
+                    cause = card.type
+                    description = card.description
+                }
+
+                if(view != null) {
+                    tvTitle.text = cause
+                    tvDetail.text = description
+                    when(cause){
+                        "Violencia Física" ->  view!!.setBackgroundColor(Color.RED)
+                        "Violencia Verbal" -> view!!.setBackgroundColor(Color.rgb(255, 69, 0))
+                        "Comportamiento Inadecuado" -> view!!.setBackgroundColor(Color.rgb(255, 20, 147))
+                        "Logro" -> view!!.setBackgroundColor(Color.YELLOW)
+                        "Empatía" -> view!!.setBackgroundColor(Color.GREEN)
+                        else-> view!!.setBackgroundColor(Color.CYAN)
+
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Error: $error", Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
+
+
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
