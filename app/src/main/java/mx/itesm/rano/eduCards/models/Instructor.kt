78f9831bc47277f06c2ccf218bfcadb4a65454e6
Alberto López <pipe.at.developer@gmail.com>
2020-10-30 00:32:37 -0600

    4.7.2: Added Username TextField and EditText to FragmentSignUp's layout.

--- app/src/main/java/mx/itesm/rano/eduCards/models/Instructor.kt
+++ app/src/main/java/mx/itesm/rano/eduCards/models/Instructor.kt
@@ -1,6 +1,7 @@
 package mx.itesm.rano.eduCards.models
 
-class Instructor(val key: String="", var name: String="", var institute: String="",
+class Instructor(val key: String="", var name: String="",
+                 var institute: String="", var username: String="",
                  var arrCourses: MutableList<String> = mutableListOf<String>(),
                  var arrGroups: MutableList<String> = mutableListOf<String>(),
                  var arrCards: MutableList<String> = mutableListOf<String>()) {
