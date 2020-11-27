7370a98b216256e5375186a13614a6e38e0179fb
Alberto López <pipe.at.developer@gmail.com>
2020-11-26 22:21:43 -0600

    Alpha 5.1 : Added friendly welcome messages.

--- app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentHome.kt
+++ app/src/main/java/mx/itesm/rano/eduCards/fragments/FragmentHome.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_home.*
 import mx.itesm.rano.eduCards.R
 
 class FragmentHome : Fragment() {
 
-    private val p = mutableListOf<String>("Buen día","suerte!","queso")
+    private val messages = mutableListOf<String>(
+        "Buen día", "¡Suerte!", "¿Queso?",
+        "Espero que estés bien", "Taquitos", "Godspeed, Watts Humphrey",
+        "Bienvenido", "¿Dónde estabas?", "¿Con quién hablabas?",
+        "¡Gracias, Tuxe!", "¡Gracias, Atena!", "¡Gracias, Frank!",
+        "¡Gracias, Lola!", "Listo para trabajar", "Sasquatch",
+        "Ameno", "Descarga D-DEF", "Gracias, RMRomán",
+        "Hecho por Raíl Ortíz Mateos", "Hecho por Nataly P. López", "Hecho por Oswaldo Morales",
+        "Hecho por Alberto López Reyes", "Hecho por RANO", "Quality Assurance, baby!"
+    )
+    lateinit var root: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +32,18 @@ class FragmentHome : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        changePhrases()
-        return inflater.inflate(R.layout.fragment_home, container, false)
-
-
+        root = inflater.inflate(R.layout.fragment_home, container, false)
+        setLayoutVariables()
+        return root
     }
 
-    private fun changePhrases(){
-        val random = (0 until (p.size)).random()
-        tvSubtitle.text = p[random]
-
+    private fun setLayoutVariables() {
+        setWelcomeMessage()
     }
 
-
-
-
-
-
+    private fun setWelcomeMessage(){
+        val random = (0 until (messages.size - 1)).random()
+        val tvSubtitle = root.findViewById(R.id.tvSubtitle) as TextView
+        tvSubtitle.text = messages.get(random)
+    }
 }
